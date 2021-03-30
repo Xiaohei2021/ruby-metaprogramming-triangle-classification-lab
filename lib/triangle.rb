cf692be8d9987978b408d3db6c73eae1ada56109
Leon <hualiang.tian@gmail.com>
2021-03-29 21:33:21 -0400

    commit tree c7d2a06bbc507aafa6d349d1153b2d39ae8d6024

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
+  end
+
+
+  def kind
+    proper_triangle
+      if a == b && b == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+    def proper_triangle
+      proper_sides = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each do |side|
+        proper_sides << false if side <= 0 
+      raise TriangleError if proper_sides.include?(false)
+      end
+    end
+
+  class TriangleError < StandardError
+  end
+
 end
